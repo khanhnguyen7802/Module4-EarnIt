30f8e12893776e6d60b0c9212f41c0d9f689e813
indigo <indigo.carelsz@gmail.com>
2023-06-07 10:07:58 +0200

    Changed login functionality to Javascript Fetch variant

--- src/main/java/nl/utwente/di/first/resources/LoginResource.java
+++ src/main/java/nl/utwente/di/first/resources/LoginResource.java
@@ -16,8 +16,9 @@ import java.sql.SQLException;
 public class LoginResource {
     
     @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public void attemptLogin(@Context HttpServletRequest req, @FormParam("email") String email, @FormParam("password") String password) {
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String attemptLogin(@Context HttpServletRequest req, @FormParam("email") String email, @FormParam("password") String password) {
         System.out.printf("A user tried to log in with the credentials: email: %s, password: %s%n", email, password);
         User user = new User();
         user.setEmail(email);
@@ -30,20 +31,10 @@ public class LoginResource {
                 session.setAttribute("role", role);
                 session.setAttribute("email", email);
             }
-            switch(role) {
-                case "COMPANY":
-                    System.out.println("A Company was found with the given credentials.");
-                    break;
-                case "STUDENT":
-                    System.out.println("A Student was found with the given credentials.");
-                    break;
-                default:
-                    System.out.println("There was no entry found for this specific tuple of credentials, either the email/password was incorrect, or there exists no account.");
-                    req.setAttribute("errMessage", "Wrong email/password. Please try again.");
-            }
+            return role;
         } catch (SQLException e) {
             // TODO handle exception correctly, since the throwing of RuntimeExceptions should be kept to a minimum.
-            throw new RuntimeException(e);
+            return "Bullshit";
         }
     }
 }
---
+++ src/main/webapp/assets/js/loginScript.js
@@ -0,0 +1,38 @@
+const submit_button = document.getElementById("submit-login");
+const err_msg = document.getElementById("err-msg");
+const email_field = document.getElementById("email");
+const password_field = document.getElementById("password");
+
+submit_button.addEventListener("click", function () {
+    if (email_field.value === "" || password_field.value === "") {
+        err_msg.innerHTML = "Please input both a username and a password to login."
+    } else {
+        let user = {
+            email: email_field.value,
+            password: password_field.value
+        };
+        fetch(window.location.origin + "/earnit/api/login", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(user)
+        }).then(response => {
+            if (!response.ok) throw new Error(`HTTP Error! Status: ${response.status}`);
+            return response.text();
+        }).then(data => {
+            if (data === "INVALID") {
+                // No redirect here
+                err_msg.innerHTML = "The given credentials were invalid. Check spelling or create an account.";
+            } else if (data === "STUDENT") {
+                // Redirect to the student homepage
+                err_msg.innerHTML = "Successfully logged in as a student";
+            } else if (data === "COMPANY") {
+                // Redirect to the company homepage
+                err_msg.innerHTML = "Successfully logged in as a company";
+            } else {
+                err_msg.innerHTML = "Idk wtf you did but you messed some shit up.";
+            }
+        })
+    }
+});
