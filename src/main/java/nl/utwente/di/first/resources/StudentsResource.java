f68d6dcaa2bf4d0c7d085fcedd35b41d83382b1d
minhhieu <minhhieuchu1012@gmail.com>
2023-06-11 01:02:38 +0200

    Small adjustments

--- src/main/java/nl/utwente/di/first/resources/StudentsResource.java
+++ src/main/java/nl/utwente/di/first/resources/StudentsResource.java
@@ -8,16 +8,15 @@ import nl.utwente.di.first.dao.UserDAO;
 import nl.utwente.di.first.model.Student;
 
 import java.util.ArrayList;
+import java.util.List;
 
+@Path("/students")
 public class StudentsResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Student> getAllStudents() {
+    public List<Student> getAllStudents() {
         return UserDAO.instance.getStudentList();
     }
 
-    @GET
-    @Path("{studentID}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public
+
 }
