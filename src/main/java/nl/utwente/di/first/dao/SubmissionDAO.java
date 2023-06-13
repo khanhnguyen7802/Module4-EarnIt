8c4915ccc71caf88ed866cca2199604f15f6717e
minhhieu <minhhieuchu1012@gmail.com>
2023-06-13 23:02:26 +0200

    Replaced jobs.html

--- src/main/java/nl/utwente/di/first/dao/SubmissionDAO.java
+++ src/main/java/nl/utwente/di/first/dao/SubmissionDAO.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public enum SubmissionDAO {
@@ -16,19 +17,26 @@ public enum SubmissionDAO {
     private SubmissionDAO() {
 
     }
-    public Submission getSubmission(String sid, String cid, LocalDate date) {
+    public List<Submission> getSubmissions(String email) {
         try {
             Connection connection = DBConnection.createConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(
                     "SELECT DISTINCT *" +
-                            "FROM submission" +
-                            "WHERE email = ?"
+                            "FROM Submission s, User u " +
+                            "WHERE u.email = ? AND (u.id = s.sid OR u.id = s.cid)"
             );
-//            preparedStatement.setString(1, email);
+            preparedStatement.setString(1, email);
             ResultSet resultSet = preparedStatement.executeQuery();
-            Submission submission = new Submission();
-
-            return submission;
+            List<Submission> submissions = new ArrayList<>();
+            while (resultSet.next()) {
+                Submission submission = new Submission();
+                submission.setComment(resultSet.getString("comment"));
+                submission.setDate(resultSet.getString("worked_date"));
+                submission.setStatus(resultSet.getString("status"));
+                submission.setHours(resultSet.getInt("hours"));
+                submissions.add(submission);
+            }
+            return submissions;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
---
+++ src/main/java/nl/utwente/di/first/resources/SubmissionResource.java
@@ -0,0 +1,49 @@
+package nl.utwente.di.first.resources;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import nl.utwente.di.first.dao.SubmissionDAO;
+import nl.utwente.di.first.model.Submission;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/submissions")
+public class SubmissionResource {
+    @Context
+    HttpServletRequest request;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Submission> getSubmissions() {
+        String role = request.getAttribute("role").toString();
+        if (role.equals("STUDENT")) {
+            //TODO: return SubmissionDAO.instance.getStudentSubmissions();
+            return null;
+        } else if (role.equals("COMPANY")) {
+            //TODO: return SubmissionDAO.instance.getCompanySubmissions();
+            return null;
+        }
+        return new ArrayList<>();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void addSubmission(
+            @FormParam("hours") int hours,
+            @FormParam("comment") String comment,
+            @FormParam("date") String date
+    ){
+        //Input date should be in format yyyy-MM-dd
+        //New submission has empty status (unconfirmed submission)
+        Submission submission = new Submission();
+        submission.setHours(hours);
+        submission.setComment(comment);
+        submission.setDate(date);
+        SubmissionDAO.instance.addSubmission(submission);
+    }
+
+
+}
