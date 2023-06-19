7b8d73d8091cc669d37477047ada6c5572363ff4
Khanh Nguyen <nguyenhoangnamkhanh@student.utwente.nl>
2023-06-19 07:25:00 +0200

    edit student list and (view details and job title of each student) still need to be discussed more

--- src/main/webapp/assets/js/studentList.js
+++ src/main/webapp/assets/js/studentList.js
@@ -1,22 +1,61 @@
+const studentTemplate = document.getElementById("student-item");
 const ul = document.getElementById("list");
-const template = document.getElementById("student-item")
 
-
-$(window).on("load", function() {
+$(window).on("load", async function () {
     fetch(window.location.origin + "/earnit/api/students")
         .then(response => {
-            if (!response.ok) throw new Error("HTTP Error! Status: " + response.status);
-            return response.json();
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Request failed with status ' + response.status);
+            }
         })
         .then(json => {
             console.log(json);
             if ("content" in document.createElement("template")) {
                 for (let item of json) {
-                    const new_student = template.content.cloneNode(true);
-                    let student_name = new_student.querySelector(".student_name")
-                    student_name.textContent = item.name;
-                    ul.appendChild(new_student);
+                    console.log(item);
+                    const new_item = studentTemplate.content.cloneNode(true);
+
+                    let student_name = new_item.querySelector(".student_name");
+                    let job_name = new_item.querySelector(".job_title");
+                    student_name.innerHTML = item.name;
+
+                    ul.append(new_item);
+
                 }
             }
+        })
+});
+
+function getJobTitle() {
+    fetch(window.location.origin + "/earnit/api/companies/${companyEmail}")
+        .then(response => {
+        if (response.ok) {
+            window.location.href = window.location.origin + "/earnit/login"
+        } else {
+            console.log('Failed to logout');
+        }
+    })
+        .catch(error => {
+            // An error occurred during the fetch request
+            console.log('Request error:', error);
         });
-})
+}
+
+function logout() {
+    fetch(window.location.origin + "/earnit/api/logout", {
+        method: "POST"
+    }).then(response => {
+        if (response.ok) {
+            window.location.href = window.location.origin + "/earnit/login"
+        } else {
+            console.log('Failed to logout');
+        }
+    })
+        .catch(error => {
+            // An error occurred during the fetch request
+            console.log('Request error:', error);
+        });
+}
+
