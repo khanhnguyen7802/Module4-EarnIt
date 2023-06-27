02ae18a16e5c5a35164efe38a02b65b0631edb02
s2966174 <buiduccuong@student.utwente.nl>
2023-06-27 22:47:21 +0200

    upload js file for savechanges student profile

---
+++ src/main/webapp/assets/js/studentProfile.js
@@ -0,0 +1,59 @@
+const saveChanges = document.getElementById("save_changes");
+const studentUniversity = document.getElementById("university");
+const studentStudy = document.getElementById("study");
+const studentSkills = document.getElementById("skills");
+const emailField = document.getElementById("email");
+const passwordField = document.getElementById("newpassword");
+const confirmPassword = document.getElementById("comfirmpassword"); // Corrected ID
+
+// Initial data
+let initialData = {
+    email: emailField.value,
+    password: passwordField.value,
+    university: studentUniversity.value,
+    study: studentStudy.value,
+    skills: studentSkills.value
+};
+
+saveChanges.addEventListener("click", function() {
+    let user = {};
+
+    if (passwordField.value !== confirmPassword.value) {
+        alert("Passwords do not match!");
+        return;
+    }
+
+    user.email = emailField.value;
+    user.password = passwordField.value;
+    user.university = studentUniversity.value;
+    user.study = studentStudy.value;
+    user.skills = studentSkills.value;
+
+    // Check if there are any changes
+    if (JSON.stringify(user) === JSON.stringify(initialData)) {
+        alert("No changes detected.");
+        return;
+    }
+
+    if (student_tab.ariaSelected === "true") {
+        fetch(window.location.origin + "/earnit/api/students", {
+            method: "PUT",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => {
+                if (!response.ok) throw new Error("HTTP Error! Status: " + response.status);
+                return response.text();
+            })
+            .then(data => {
+                // TODO: Replace this with redirect
+                if (data === "SUCCESS") alert("Success!");
+                if (data === "FAILURE") alert("Oof, failure...");
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+});
