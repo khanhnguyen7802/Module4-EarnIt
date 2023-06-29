5a510affb2946088c3e8a90acb5c5e5c51917b6f
minhhieu <minhhieuchu1012@gmail.com>
2023-06-29 22:57:43 +0200

    Profile pages and test clients

--- src/main/java/nl/utwente/di/first/client/TestCompanyClient.java
+++ src/main/java/nl/utwente/di/first/client/TestCompanyClient.java
@@ -1,5 +1,44 @@
 package nl.utwente.di.first.client;
 
-public class TestCompanyClient {
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.UriBuilder;
+import nl.utwente.di.first.model.Company;
+import nl.utwente.di.first.model.User;
 
+import java.net.URI;
+
+public class TestCompanyClient {
+    private static URI getBaseURI() {
+        return UriBuilder.fromUri("http://localhost:8080/earnit").build();
+    }
+
+    public static void main(String[] args) {
+        String url_pattern = "api";
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(getBaseURI());
+
+        //Login as a company, "COMPANY" result is expected
+        User company = new Company();
+        company.setEmail("company1@gmail.com");
+        company.setPassword("company1");
+        System.out.println("Logging in as a company...");
+        String results = target.path(url_pattern).path("login")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(company, MediaType.APPLICATION_JSON), String.class);
+        System.out.println("Response:\n" + results);
+
+        //Try to get the list of students
+        System.out.println("Fetching students");
+        results = target.path(url_pattern).path("students")
+                .request(MediaType.APPLICATION_JSON).get(String.class);
+        System.out.println("Response:\n" + results);
+
+        //Try to get the list of submission in the current week
+
+        client.close();
+    }
 }
--- src/main/java/nl/utwente/di/first/client/TestStudentClient.java
+++ src/main/java/nl/utwente/di/first/client/TestStudentClient.java
@@ -15,27 +15,29 @@ public class TestStudentClient {
     }
     public static void main(String[] args) {
         String url_pattern = "api";
-        String path = "students";
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target(getBaseURI());
-        studentLogin(target);
-        requestCompanyList(target);
-        client.close();
-    }
 
-    public static void studentLogin(WebTarget target) {
+        //Login as a student, a response "STUDENT" is expected
+        //TODO: Maybe integrate with JUnit
         User student = new User();
         student.setEmail("student1@gmail.com");
         student.setPassword("student1");
         System.out.println("Logging in as student...");
-        Response response = target.request(MediaType.APPLICATION_JSON)
-                        .post(Entity.json(student));
-        System.out.println("Response: " + response);
-    }
-    private static void requestCompanyList(WebTarget target) {
-        System.out.println("Requesting company list");
-        Response response = target.request(MediaType.APPLICATION_JSON)
-                .get();
-        System.out.println("Response: " + response);
+        String results = target.path(url_pattern).path("login")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(student, MediaType.APPLICATION_JSON), String.class);
+        System.out.println("Response:\n" + results);
+
+        //Try to get the list of company
+        System.out.println("Fetching companies");
+        results = target.path(url_pattern).path("companies")
+                        .request(MediaType.APPLICATION_JSON).get(String.class);
+        System.out.println("Response:\n" + results);
+
+        //Try to get the list of submission in the current week
+
+        //TODO: Test submission-related APIs
+        client.close();
     }
 }
