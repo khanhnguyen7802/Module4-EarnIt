46fa8acbbbb66875501625cf121da56ec69d8ecd
Khanh Nguyen <nguyenhoangnamkhanh@student.utwente.nl>
2024-02-24 14:54:01 +0100

    change to localhost

--- src/main/java/nl/utwente/di/first/util/DBConnection.java
+++ src/main/java/nl/utwente/di/first/util/DBConnection.java
@@ -1,20 +1,18 @@
 package nl.utwente.di.first.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBConnection {
-    public static String DB_HOST = "bronto.ewi.utwente.nl";
-    public static String DB_NAME = "dab_di22232b_116";
-    public static String DB_URL = "jdbc:postgresql://" + DB_HOST + ":5432/" + DB_NAME + "?currentSchema=earnit";
+    public static String DB_HOST = "localhost";
+    public static String DB_NAME = "earnit";
+    public static String DB_URL = "jdbc:postgresql://" + DB_HOST + ":5432/" + DB_NAME;
 
     public static Connection createConnection() {
         Connection connection = null;
 
         try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(DB_URL, "dab_di22232b_116", "FsrnTKGS3P+pC/a+");
+            connection = DriverManager.getConnection(DB_URL, "postgres", "root");
         } catch (SQLException e) {
             System.err.println("Error connecting: " + e);
         } catch (ClassNotFoundException e) {
@@ -22,6 +20,21 @@ public class DBConnection {
         }
 
         return connection;
+    }
+
+    // for testing only
+    public static void main(String[] args) {
+        try (Connection connection = DBConnection.createConnection();) {
+
+            String query = "SELECT * " +
+                    "FROM company";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            System.out.println("ok");
+        } catch (SQLException e) {
+            // FIXME Runtime exceptions should be thrown as little as possible, error messages are much preferred.
+            throw new RuntimeException(e);
+        }
     }
 
 
